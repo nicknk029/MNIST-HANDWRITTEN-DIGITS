2d6cf53946a7cb629c3af644f4e83d84baaab848
Nirmalkumar Telagalapalli <51757248+nicknk029@users.noreply.github.com>
2019-06-25 18:19:26 -0500

    Update CNN.py

--- CNN.py
+++ CNN.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense, Flatten, Dropout
-from keras.layers.convolutional import Conv2D,MaxPooling2D
+from keras.layers.convolutional import Conv2D, MaxPooling2D
 from keras.utils import np_utils
 
 # LOADING THE DATASET
@@ -17,30 +17,30 @@ print( 'Test size:', x_test.shape )
 
 # IMAGE PLOT WITH LABEL 
 
-plt.figure(figsize=(15,15))
+plt.figure( figsize= ( 15, 15 ) )
 
-for i in range(15) :   
-    plt.subplot(3,5, i+1)
+for i in range( 15 ) :   
+    plt.subplot( 3, 5, i+1 )
     plt.imshow( x_train[i], cmap='gray' )
-    plt.title( y_train[i], fontsize = 20)
+    plt.title( y_train[i], fontsize = 20 )
     
 # RESHAPING
 
-X_train = x_train.reshape(x_train.shape[0], 1, 28, 28)
-X_test = x_test.reshape(x_test.shape[0], 1, 28, 28)
+X_train = x_train.reshape( x_train.shape[0], 1, 28, 28 )
+X_test = x_test.reshape( x_test.shape[0], 1, 28, 28 )
 
 # CONVERT TO FLOAT SO THAT WE WILL HAVE DECIMAL POINTS AFTER DIVISION
 
-X_train = X_train.astype('float32')
-X_test = X_test.astype('float32')
-print('X_train after reshape:', X_train.shape)
+X_train = X_train.astype( 'float32' )
+X_test = X_test.astype( 'float32' )
+print( 'X_train after reshape:', X_train.shape )
 
 # NORMALIZING- 255 IS THE HIGHEST VALUE
 
 X_train = X_train / 255
 X_test = X_test / 255
-print(np.max(X_train[0]))
-print(np.min(X_train[0]))
+print( np.max( X_train[0] ) )
+print( np.min( X_train[0] ) )
 
 
 # CONVERT Y VALUES TO CATEGORICAL
@@ -49,7 +49,7 @@ y_train = np_utils.to_categorical( y_train )
 y_test = np_utils.to_categorical( y_test )
 num_classes = y_test.shape[1]
 
-# 3 LAYER CNN
+# CNN WITH 3 LAYERS
 
 model = Sequential()
 model.add( Conv2D( 32, ( 3, 3 ), input_shape = ( 1, 28, 28 ), activation = 'relu', data_format = 'channels_first') )
